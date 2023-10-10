587ec9395f414bc3057153ad1f0547d87bf39d5f
thiago <thiagoyuske@outlook.com>
2023-10-10 16:33:22 -0300

    feat(User): Creating table and Entity

---
+++ src/main/java/br/com/thiagoyuske/todolist/user/UserController.java
@@ -0,0 +1,34 @@
+package br.com.thiagoyuske.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    /*
+     * Utilizando o @Requestbody, informamos que vem de uma requisição já como um
+     * objeto de UserModel
+     */
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+}
